03062217fd0778897592072f6a8cf4524250608a
Mel1897 <martinsdossantosmelissa093@gmail.com>
2025-05-21 14:35:02 -0300

    Add files via upload

---
+++ ConversorTemperatura1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.conversortemperatura1;
+
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+public class ConversorTemperatura1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
