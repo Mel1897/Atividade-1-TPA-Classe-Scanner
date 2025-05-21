65cc8e1abf2791dda4790bf791c6870bb67d63a8
Mel1897 <martinsdossantosmelissa093@gmail.com>
2025-05-21 14:34:11 -0300

    Add files via upload

---
+++ CalculadoraMedia1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadoramedia1;
+
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+public class CalculadoraMedia1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
